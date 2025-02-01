aa594c53c6ea26a5e3d7632bf6848f8d7e63b37d
Madumarov Davron <menaceteacher@gmail.com>
2025-02-02 02:48:05 +0500

    add

--- src/main/java/org/example/PongGame.java
+++ src/main/java/org/example/PongGame.java
@@ -57,117 +57,117 @@ public class PongGame extends JPanel implements KeyListener, ActionListener {
             g.drawString("Game Over!", WIDTH / 2 - 120, HEIGHT / 2);
         }
     }
-//
-//    @Override
-//    public void actionPerformed(ActionEvent e) {
-//        // Agar o'yin tugamagan bo'lsa, o'yinni davom ettirish
-//        if (ballX <= 0) {
-//            rightScore++; // O'ng o'yinchi gol oladi
-//            resetBall(); // To'pni qayta boshlash
-//        } else if (ballX >= WIDTH - 15) {
-//            leftScore++; // Chap o'yinchi gol oladi
-//            resetBall(); // To'pni qayta boshlash
-//        } else {
-//            moveBall(); // To'pni harakatlantirish
-//            movePads(); // Padlarni harakatlantirish
-//            checkCollisions(); // To'qnashuvlarni tekshirish
-//        }
-//        repaint(); // O'yin maydonini yangilash
-//    }
-//
-//    private void moveBall() {
-//        ballX += ballDX;
-//        ballY += ballDY;
-//
-//        // To'pning yuqori va pastki chegaralardan to'qnashishi
-//        if (ballY <= 0 || ballY >= HEIGHT - 15) {
-//            ballDY = -ballDY; // Yuqoriga yoki pastga to'qnashganda
-//        }
-//    }
-//
-//    private void movePads() {
-//        // Chap padni harakatlantirish
-//        if (leftUp && leftPadY > 0) {
-//            leftPadY -= 4; // Yuqoriga harakat
-//        }
-//        if (leftDown && leftPadY < HEIGHT - PAD_HEIGHT) {
-//            leftPadY += 4; // Pastga harakat
-//        }
-//
-//        // O'ng padni harakatlantirish
-//        if (rightUp && rightPadY > 0) {
-//            rightPadY -= 4; // Yuqoriga harakat
-//        }
-//        if (rightDown && rightPadY < HEIGHT - PAD_HEIGHT) {
-//            rightPadY += 4; // Pastga harakat
-//        }
-//    }
-//
-//    private void checkCollisions() {
-//        // Chap pad bilan to'qnashuvni tekshirish
-//        if (ballX <= 45 && ballY + 15 >= leftPadY && ballY <= leftPadY + PAD_HEIGHT) {
-//            ballDX = -ballDX; // Chap pad bilan to'qnashganda
-//        }
-//
-//        // O'ng pad bilan to'qnashuvni tekshirish
-//        if (ballX >= WIDTH - 45 && ballY + 15 >= rightPadY && ballY <= rightPadY + PAD_HEIGHT) {
-//            ballDX = -ballDX; // O'ng pad bilan to'qnashganda
-//        }
-//    }
-//
-//    private void resetBall() {
-//        // To'pni markazga qaytarish
-//        ballX = WIDTH / 2 - 7;
-//        ballY = HEIGHT / 2 - 7;
-//        ballDX = -ballDX; // To'pning yo'nalishini o'zgartirish
-//        ballDY = 2; // To'pning yuqori yoki pastga harakatlanishi
-//    }
-//
-//
-//    @Override
-//    public void keyPressed(KeyEvent e) {
-//        int key = e.getKeyCode();
-//        if (key == KeyEvent.VK_W) {
-//            leftUp = true;
-//        }
-//        if (key == KeyEvent.VK_S) {
-//            leftDown = true;
-//        }
-//        if (key == KeyEvent.VK_UP) {
-//            rightUp = true;
-//        }
-//        if (key == KeyEvent.VK_DOWN) {
-//            rightDown = true;
-//        }
-//    }
-//
-//    @Override
-//    public void keyReleased(KeyEvent e) {
-//        int key = e.getKeyCode();
-//        if (key == KeyEvent.VK_W) {
-//            leftUp = false;
-//        }
-//        if (key == KeyEvent.VK_S) {
-//            leftDown = false;
-//        }
-//        if (key == KeyEvent.VK_UP) {
-//            rightUp = false;
-//        }
-//        if (key == KeyEvent.VK_DOWN) {
-//            rightDown = false;
-//        }
-//    }
-//
-//    @Override
-//    public void keyTyped(KeyEvent e) {}
-//
-//    public static void main(String[] args) {
-//        JFrame frame = new JFrame("Pong Game");
-//        PongGame gamePanel = new PongGame();
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.add(gamePanel);
-//        frame.pack();
-//        frame.setLocationRelativeTo(null);
-//        frame.setVisible(true);
-//    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (ballX <= 0) {
+            rightScore++;
+            resetBall();
+        } else if (ballX >= WIDTH - 15) {
+            leftScore++;
+            resetBall();
+        } else {
+            moveBall();
+            movePads();
+            checkCollisions();
+        }
+        repaint();
+    }
+
+    private void moveBall() {
+        ballX += ballDX;
+        ballY += ballDY;
+
+
+        if (ballY <= 0 || ballY >= HEIGHT - 15) {
+            ballDY = -ballDY;
+        }
+    }
+
+    private void movePads() {
+
+        if (leftUp && leftPadY > 0) {
+            leftPadY -= 4;
+        }
+        if (leftDown && leftPadY < HEIGHT - PAD_HEIGHT) {
+            leftPadY += 4;
+        }
+
+
+        if (rightUp && rightPadY > 0) {
+            rightPadY -= 4;
+        }
+        if (rightDown && rightPadY < HEIGHT - PAD_HEIGHT) {
+            rightPadY += 4;
+        }
+    }
+
+    private void checkCollisions() {
+
+        if (ballX <= 45 && ballY + 15 >= leftPadY && ballY <= leftPadY + PAD_HEIGHT) {
+            ballDX = -ballDX;
+        }
+
+
+        if (ballX >= WIDTH - 45 && ballY + 15 >= rightPadY && ballY <= rightPadY + PAD_HEIGHT) {
+            ballDX = -ballDX;
+        }
+    }
+
+    private void resetBall() {
+
+        ballX = WIDTH / 2 - 7;
+        ballY = HEIGHT / 2 - 7;
+        ballDX = -ballDX;
+        ballDY = 2;
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_W) {
+            leftUp = true;
+        }
+        if (key == KeyEvent.VK_S) {
+            leftDown = true;
+        }
+        if (key == KeyEvent.VK_UP) {
+            rightUp = true;
+        }
+        if (key == KeyEvent.VK_DOWN) {
+            rightDown = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_W) {
+            leftUp = false;
+        }
+        if (key == KeyEvent.VK_S) {
+            leftDown = false;
+        }
+        if (key == KeyEvent.VK_UP) {
+            rightUp = false;
+        }
+        if (key == KeyEvent.VK_DOWN) {
+            rightDown = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Pong Game");
+        PongGame gamePanel = new PongGame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
